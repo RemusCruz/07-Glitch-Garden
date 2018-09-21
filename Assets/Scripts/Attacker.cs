27ba5e783b39c6d638ae50354b2cff0ae3d67afa
RAKI-PC\Raki <remus.cruz@gmail.com>
2018-09-21 03:16:50 +0100

    GG.01

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour {
+
+    [Range (-1f, 1.5f)]
+    public float currentSpeed;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
+        myRigidbody.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);	
+	}
+
+    public void SetSpeed (float speed)
+    {
+        currentSpeed = speed;
+    }
+
+    public void StrikeCurrentTarget(float damage)
+    {
+        Debug.Log(name + " caused danage: " + damage);
+    }
+
+}
